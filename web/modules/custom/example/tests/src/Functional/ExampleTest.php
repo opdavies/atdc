44adee585b946cd6e039ba1d91ad20726af1445e
Oliver Davies <oliver@oliverdavies.uk>
2023-12-26 01:35:56 +0000

    Test Drupal pages

--- web/modules/custom/example/tests/src/Functional/ExampleTest.php
+++ web/modules/custom/example/tests/src/Functional/ExampleTest.php
@@ -3,13 +3,24 @@
 namespace Drupal\Tests\example\Functional;
 
 use Drupal\Tests\BrowserTestBase;
+use Symfony\Component\HttpFoundation\Response;
 
 class ExampleTest extends BrowserTestBase {
 
   protected $defaultTheme = 'stark';
 
-  public function testBasic(): void {
-    self::assertTrue(TRUE);
+  public function testFrontPage(): void {
+    $this->drupalGet('/');
+
+    $assert = $this->assertSession();
+    $assert->statusCodeEquals(Response::HTTP_OK);
+  }
+
+  public function testAdminPage(): void {
+    $this->drupalGet('/admin');
+
+    $assert = $this->assertSession();
+    $assert->statusCodeEquals(Response::HTTP_FORBIDDEN);
   }
 
 }
