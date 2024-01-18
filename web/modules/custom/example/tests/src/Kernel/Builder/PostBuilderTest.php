e6caccc1b4f18679c6a353bb0ed9c983b15ca8c6
Oliver Davies <oliver@oliverdavies.uk>
2024-01-18 07:14:35 +0000

    Add empty test

---
+++ web/modules/custom/example/tests/src/Kernel/Builder/PostBuilderTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\Tests\example\Kernel\Builder;
+
+use Drupal\KernelTests\Core\Entity\EntityKernelTestBase;
+
+/**
+ * @group lessons
+ */
+final class PostBuilderTest extends EntityKernelTestBase {
+
+}
