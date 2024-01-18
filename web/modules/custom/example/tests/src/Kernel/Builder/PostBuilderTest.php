2b3f98c43d0d4dc572d7649fa9a81e06ff5bf95b
Oliver Davies <oliver@oliverdavies.uk>
2024-01-18 07:32:47 +0000

    Add published post test

--- web/modules/custom/example/tests/src/Kernel/Builder/PostBuilderTest.php
+++ web/modules/custom/example/tests/src/Kernel/Builder/PostBuilderTest.php
@@ -2,11 +2,27 @@
 
 namespace Drupal\Tests\example\Kernel\Builder;
 
+use Drupal\example\Builder\PostBuilder;
 use Drupal\KernelTests\Core\Entity\EntityKernelTestBase;
+use Drupal\node\NodeInterface;
 
 /**
  * @group lessons
  */
 final class PostBuilderTest extends EntityKernelTestBase {
+
+  protected static $modules = ['node'];
+
+  /** @test */
+  public function it_returns_a_published_post(): void {
+    $node = PostBuilder::create()
+      ->setTitle('test')
+      ->isPublished()
+      ->getPost();
+
+    self::assertInstanceOf(NodeInterface::class, $node);
+    self::assertSame('post', $node->bundle());
+    self::assertTrue($node->isPublished());
+  }
 
 }
