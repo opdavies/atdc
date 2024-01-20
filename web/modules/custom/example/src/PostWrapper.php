2daeca7f8ee1e000400139011b5035161b9b2798
Oliver Davies <oliver@oliverdavies.uk>
2024-01-20 09:29:53 +0000

    Throw an Exception if the bundle type is incorrect

--- web/modules/custom/example/src/PostWrapper.php
+++ web/modules/custom/example/src/PostWrapper.php
@@ -6,7 +6,13 @@ use Drupal\node\NodeInterface;
 
 final class PostWrapper {
 
+  /**
+   * @throws \InvalidArgumentException
+   */
   public function __construct(private NodeInterface $post) {
+    if ($post->bundle() !== 'post') {
+      throw new \InvalidArgumentException();
+    }
   }
 
   public function getType(): string {
