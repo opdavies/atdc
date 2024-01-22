c54b3efa932e68edd64cddadaecf6f4e9d2e5755
Oliver Davies <oliver@oliverdavies.uk>
2024-01-22 16:58:45 +0000

    Add PostNodeRepositoryUnitTest
    > ArgumentCountError: Too few arguments to function Drupal\example\Repository\PostNodeRepository::__construct(), 0 passed in /home/opdavies/Code/github.com/opdavies/atdc
    
    /web/modules/custom/example/tests/src/Unit/PostNodeRepositoryUnitTest.php on line 15 and exactly 1 expected

---
+++ web/modules/custom/example/tests/src/Unit/PostNodeRepositoryUnitTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Drupal\Tests\example\Unit;
+
+use Drupal\example\Repository\PostNodeRepository;
+use Drupal\Tests\UnitTestCase;
+
+/**
+ * @group lessons
+ */
+final class PostNodeRepositoryUnitTest extends UnitTestCase {
+
+  /** @test */
+  public function it_returns_posts(): void {
+    $repository = new PostNodeRepository();
+  }
+
+}
